b3d8995cfb0b56101b5fa15ef0c5baaa8321188e
egAyush <ayushpandey3269@gmail.com>
2024-04-11 21:14:06 +0530

    Initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD38sql9fbfKt8sRUD3Zrm5p3Hg7hCULVE',
+  authDomain: 'chat-web-app1-1f335.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app1-1f335-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'chat-web-app1-1f335',
+  storageBucket: 'chat-web-app1-1f335.appspot.com',
+  messagingSenderId: '104608603919',
+  appId: '1:104608603919:web:a6f9ed8477371c7012e87a',
+};
+
+const app = initializeApp(firebaseConfig);
