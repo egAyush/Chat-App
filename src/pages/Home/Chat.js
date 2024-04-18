c01f29d9d1eba4b814530e889e5098966302a1db
egAyush <ayushpandey3269@gmail.com>
2024-04-18 08:57:46 +0530

    started devolping  chat room with dynamic url using useparams

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,7 +1,41 @@
 // import React from 'react'
 
+import Messages from '../../components/chat-window/messages';
+import ChatTop from '../../components/chat-window/top';
+import ChatBottom from '../../components/chat-window/bottom';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useRooms } from '../../context/rooms.context';
+import { Loader } from 'rsuite';
+
 const Chat = () => {
-  return <div>chat</div>;
+  const { chatId } = useParams();
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-Top">
+        <ChatTop />
+      </div>
+
+      <div className="chat-middle">
+        <Messages />
+      </div>
+
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
