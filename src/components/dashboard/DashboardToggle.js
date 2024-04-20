18337c141bec3f88f8cdfa38d2e8432270643f40
egAyush <ayushpandey3269@gmail.com>
2024-04-20 09:59:19 +0530

    added realtime listner for presence on user

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -3,18 +3,26 @@ import { Button, Icon, Drawer, Alert } from 'rsuite';
 import { useMediaQuery, useModelState } from '../../misc/custom-hook';
 import Dashboard from '.';
 import { useCallback } from 'react';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModelState();
   const isMobile = useMediaQuery('(max-width : 992px)');
 
   const onSignOut = useCallback(() => {
-    auth.signOut();
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
 
-    Alert.info('Signed Out', 4000);
-
-    close();
+        Alert.info('Signed Out', 4000);
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
   }, [close]);
 
   return (
