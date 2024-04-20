899f1edaec33d26fb474f3b93fbbb9c4dd97e891
egAyush <ayushpandey3269@gmail.com>
2024-04-20 16:15:13 +0530

    added room drawer to edit the room info

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -6,6 +6,7 @@ import { ButtonToolbar, Icon } from 'rsuite';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { useMediaQuery } from '../../../misc/custom-hook';
 import RoomInfoBtnModal from './RoomInfoBtnModal';
+import EditRoomBtnDrawer from './EditRoomBtnDrawer';
 
 const Top = () => {
   const name = useCurrentRoom(v => v.name);
@@ -29,7 +30,9 @@ const Top = () => {
           <span className="text-disappear"> {name}</span>
         </h4>
 
-        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+        <ButtonToolbar className="ws-nowrap">
+          <EditRoomBtnDrawer />
+        </ButtonToolbar>
       </div>
 
       <div className="d-flex justify-content-between align-items-center">
